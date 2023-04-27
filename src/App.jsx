b9cce64d20723b69b3b02d8e4670e4e10f1c0ed4
desireebarretti <110750885+desireebarretti@users.noreply.github.com>
2023-04-27 12:54:50 -0300

    Criando os cards do formulário

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,42 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario/Formulario';
+import Time from './componentes/Time/Time';
 
 export default function App() {
+
+  const times = [
+    {
+      nome: 'Programação',
+      corPrimaria: '#57C278',
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'Data Sciense',
+      corPrimaria: '#A6D157',
+      corSecundaria: '#F0F8E2',
+    },
+    {
+      nome: 'Devops',
+      corPrimaria: '#E06B69',
+      corSecundaria: '#FDE7E8',
+    },
+    {
+      nome: 'UX e Design',
+      corPrimaria: '#D86EBF',
+      corSecundaria: '#FAE5F5',
+    },
+    {
+      nome: 'Mobile',
+      corPrimaria: '#FEBA05',
+      corSecundaria: '#FFF5D9',
+    },
+    {
+      nome: 'Inovação e Gestão',
+      corPrimaria: '#FF8A29',
+      corSecundaria: '#FFEEDF',
+    }
+  ]
 
   const [colaboradores, setColaboradores] = useState([])
 
@@ -12,9 +46,15 @@ export default function App() {
   }
 
   return (
-    <div className="App">  
+    <div className="App">
       <Banner />
-      <Formulario colaboradorCadastrado={colaborador => adicionar(colaborador)}/>
+      <Formulario colaboradorCadastrado={colaborador => adicionar(colaborador)} />
+
+      {times.map(time =>
+        <Time key={time.times}
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria} />)}
     </div>
   );
 }
