4bda5c1c97e1619af5de637ca24da1cf0a13376f
desireebarretti <110750885+desireebarretti@users.noreply.github.com>
2023-05-02 21:37:52 -0300

    Aprimorando o filter() em colaboradores

--- src/App.jsx
+++ src/App.jsx
@@ -56,7 +56,7 @@ export default function App() {
           nome={time.nome}
           corPrimaria={time.corPrimaria}
           corSecundaria={time.corSecundaria} 
-          colaboradores={colaboradores}
+          colaboradores={colaboradores.filter(colaborador => colaborador.time == time.nome)}
         />)}
 
     </div>
